7a58e8955f51fe9bec70bd50e2d3cf289a9336d8
Jess <jmerriman95@live.com>
2021-12-07 14:36:03 +1100

    ssl keys use env vars now

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ import uvicorn
 # from typing import Optional
 # from pydantic import BaseModel
 import dotenv
-# import os
+import os
 import json
-from pprint import pprint
+# from pprint import pprint
 
 dotenv.load_dotenv()
 api = FastAPI()
@@ -20,20 +20,20 @@ async def _hello(request: Request):
     return {"greeting": "hello", "subject": "subject"}
 
 
-@api.post("/callback")
-async def _callback(request: Request):
-    req = await request.json()
-    print(json.dumps(req, indent=4))
-    return {"greeting": "hello", "subject": "subject"}
+# @api.post("/callback")
+# async def _callback(request: Request):
+#     req = await request.json()
+#     print(json.dumps(req, indent=4))
+#     return {"greeting": "hello", "subject": "subject"}
 
-# @api.get("/callback")
-# async def typehint(envar: str):
-#     return {"envar": envar, "val": os.environ[envar]}
+# # @api.get("/callback")
+# # async def typehint(envar: str):
+# #     return {"envar": envar, "val": os.environ[envar]}
 
 if __name__ == '__main__':
     uvicorn.run("main:api",
                 host="0.0.0.0",
                 port=8000,
                 reload=True,
-                ssl_keyfile="../testkey.pem",
-                ssl_certfile="../testcert.pem")
+                ssl_keyfile=os.environ["SSL_KEY"],
+                ssl_certfile=os.environ["SSL_CERT"])
